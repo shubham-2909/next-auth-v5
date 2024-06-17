127ccef28e0c0997b2a9c52489fe8a81540e6cd5
Shubham Gandhi <sg9769356@gmail.com>
2024-06-17 15:49:51 +0530

    fix bug of adapter prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma";
+import { Adapter } from "next-auth/adapters"
 import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/github"
 import Resend from "next-auth/providers/resend"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
+  theme: {
+    logo: "/logo.png",
+  },
   providers: [Google, Github, Resend({
     from: "no-reply@emails.webdevka14.in"
   })],
@@ -15,5 +19,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session
     }
   },
-  adapter: PrismaAdapter(prisma)
+  adapter: PrismaAdapter(prisma) as Adapter
 })
